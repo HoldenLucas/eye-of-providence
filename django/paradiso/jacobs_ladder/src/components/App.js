b059bcdbe213e71eb914c44d53a3c1889f3d4295
HoldenLucas <hopalucas@gmail.com>
2019-03-29 19:30:12 -0400

    added material-ui react and a basic card

--- django/paradiso/jacobs_ladder/src/components/App.js
+++ django/paradiso/jacobs_ladder/src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import DataProvider from './DataProvider'
 import Table from './Table'
+import GuestCard from './GuestCard'
 
 // TODO: fix endpoint name
 const App = () => (
-  <DataProvider endpoint='api/guest/'
-    render={data => <Table data={data} />} />
+  // <DataProvider endpoint='api/guest/' render={data => <Table data={data} />} />
+  <GuestCard />
 )
 
 const wrapper = document.getElementById('app')
---
+++ django/paradiso/jacobs_ladder/src/components/GuestCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent';
+
+const GuestCard = ({}) =>
+    <Card>
+      <CardContent>
+        fuck
+      </CardContent>
+    </Card>
+
+export default GuestCard
